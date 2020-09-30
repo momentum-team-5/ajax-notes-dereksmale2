38170b07290534a3b9b189f948bdc65adb4db563
Derek Smale <dereksmale2@gmail.com>
2020-09-30 15:52:56 -0400

    up to delete

--- main.js
+++ main.js
@@ -1,35 +1,84 @@
-/* moment */
+const url = 'http://localhost:3000/notes';
+const notesList = document.querySelector('#notes-list');
 
-const url = 'http://localhost:3000/notes'
+// Render Methods
+const renderNote = (data) => {
+    const notesItemEl = document.createElement('div');
+    notesItemEl.dataset.id = data.id;
+    notesItemEl.id = `note-${data.id}`;
 
-document.addEventListener('submit', function (event) {
-    event.preventDefault()
-    const notesInput = document.querySelector('#notes-input').value
-    console.log(notesInput)
+    notesItemEl.innerHTML = `
+        <div class="note-item-title">
+            ${data.notesItem}
+        </div>
+        <div class="note-item-button-wrapper">
+            <button class="delete">delete</button>
+            <button class="edit">edit</button>
+        </div>
+    `;
 
+    notesList.appendChild(notesItemEl);
+}
+
+const renderNotes = () => {
+    fetch(url)
+    .then(res => res.json())
+    .then(notesData => {
+        for (const item of notesData) {
+            renderNote(item);
+        }
+    })
+}
+
+// API Methods
+const createNote = () => {
+    const notesInput = document.querySelector('#notes-input');
     fetch(url, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({notesItem: notesInput, created_at: moment().format()
+        body: JSON.stringify({notesItem: notesInput.value, created_at: moment().format()
         })
     })
     .then(res => res.json())
     .then(data => {
-        const notesList = document.querySelector('#new-note')
-        const notesItemEl = document.createElement('textarea')
-        notesItemEl.innerText = data.notesItem
-        notesList.appendChild(notesItemEl)
+        renderNote(data);
+        deleteText(notesInput);
     })
+}
+
+const updateNote = () => {
+
+}
+
+const deleteNote = (id) => {
+    fetch(url + '/' + id, {
+        method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            const itemToRemove = document.querySelector(`#note-${id}`)
+            itemToRemove.remove();
+        })
+}
+
+// Helper Methods
+const deleteText = el => {
+    el.value = "";
+}
+
+
+//Event Listeners
+document.addEventListener('submit', event => {
+    event.preventDefault();
+    createNote();
 })
 
-fetch(url)
-    .then(res => res.json())
-    .then(notesData => {
-        const notesList = document.querySelector('#new-note')
-        for (const item of notesData) {
-            console.log(item)
-            const notesItemEl = document.createElement('textarea')
-            notesItemEl.innerText = item.notesItem
-            notesList.appendChild(notesItemEl)
-        }
-    })
+notesList.addEventListener('click', event => {
+    if (event.target.matches('.delete')) {
+        let firstParent = event.target.parentElement;
+        deleteNote(firstParent.parentElement.dataset.id);
+    }
+})
+
+renderNotes();
+
